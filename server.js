45b8c7535f652a574bbe43709a947803df4e4a38
Fu <sophie.fu.sun@outlook.com>
2021-01-30 17:17:06 -0600

    adding port

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+// when run locally, we use PORT 300, for deployment, we need use the port we get
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
 })
